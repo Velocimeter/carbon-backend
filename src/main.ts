7f90fbaf930de35b0181c67603f41c2fe3201177
0xBiggy <dmgwizz@gmail.com>
2025-05-03 11:19:17 +1000

    revert http2

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggingInterceptor } from './logging.interceptor';
-import * as fs from 'fs';
-import * as path from 'path';
 
 async function bootstrap() {
-  // HTTP/2 options
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, '../ssl/server.key')),
-    cert: fs.readFileSync(path.join(__dirname, '../ssl/server.crt')),
-  };
-
   const app = await NestFactory.create(AppModule, { 
     cors: {
       origin: true, // You can specify specific origins instead of true
@@ -21,7 +13,6 @@ async function bootstrap() {
       credentials: true,
     },
     logger: ['error', 'warn', 'log', 'debug', 'verbose'], // Enable all log levels
-    httpsOptions, // Enable HTTPS/HTTP2
   });
   
   // Enable API versioning
