d2be9909f7cf5839f3ecaefacbb721af6a0637a7
0xBiggy <dmgwizz@gmail.com>
2025-05-04 23:29:34 +1000

    fix healthcheck

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
-import { UpdaterModule } from '../updater/updater.module';
-import { DeploymentModule } from '../deployment/deployment.module';
 
 @Module({
-  imports: [
-    TerminusModule,
-    UpdaterModule,
-    DeploymentModule,
-  ],
+  imports: [TerminusModule],
   controllers: [HealthController],
 })
 export class HealthModule {} 
